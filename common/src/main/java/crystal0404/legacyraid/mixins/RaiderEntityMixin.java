de18dc0e6b587ad325f7b06a94a5b7c76d308617
Crystal0404 <syh040325@163.com>
2024-12-08 00:14:17 +0800

    LICENSE

--- common/src/main/java/crystal0404/legacyraid/mixins/RaiderEntityMixin.java
+++ common/src/main/java/crystal0404/legacyraid/mixins/RaiderEntityMixin.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the LegacyRaid project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2024  Crystal0404 and contributors
+ *
+ * LegacyRaid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * LegacyRaid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LegacyRaid.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package crystal0404.legacyraid.mixins;
 
 import net.minecraft.entity.Entity;
